efdf4720eaed7f4a9fda4c697b1405db53ab9393
Denis Žoljom <denis.zoljom@gmail.com>
2020-05-27 20:26:08 +0200

    Move assets to webpack encore

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,6 +9,19 @@
 import '../css/app.css';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-// import $ from 'jquery';
+import $ from 'jquery';
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+const $container = $('.js-vote-arrows');
+
+$container.find('a').on('click', function (e) {
+  e.preventDefault();
+  const $link = $(e.currentTarget);
+
+  $.ajax({
+    url: '/comments/10/vote/'+$link.data('direction'),
+    method: 'POST'
+  }).then(function (data) {
+    $container.find('.js-vote-total').text(data.votes);
+  });
+});
+
