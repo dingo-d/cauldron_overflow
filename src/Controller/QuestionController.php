ab77a1e80e5ff7ff593ae2b19a57aee9e01427b1
Denis Žoljom <denis.zoljom@gmail.com>
2020-05-31 10:28:29 +0200

    Add markdown parser to show method

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -11,6 +11,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
@@ -42,19 +43,21 @@ class QuestionController extends AbstractController
   /**
    * @Route("/questions/{slug}", name="app_question_show")
    */
-  public function show($slug)
+  public function show($slug, MarkdownParserInterface $markdownParser)
   {
     $answers = [
-      'Make sure your cat is sitting purrrfectly still 🤣',
+      'Make sure your cat is sitting `purrrfectly` still 🤣',
       'Honestly, I like furry shoes better than MY cat',
       'Maybe... try saying the spell backwards',
     ];
 
     $questionText = 'I\'ve been turned into a cat, any thoughts on how to turn back? While I\'m **adorable**, I don\'t really care for cat food.';
 
+    $parsedQuestionText = $markdownParser->transformMarkdown($questionText);
+
     return $this->render('question/show.html.twig', [
       'question' => ucwords(str_replace('-', ' ', $slug)),
-      'questionText' => $questionText,
+      'questionText' => $parsedQuestionText,
       'answers'  => $answers,
     ]);
   }
