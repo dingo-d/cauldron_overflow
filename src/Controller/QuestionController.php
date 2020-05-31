6dbb4bec70ddd5f126e9a85dd175831ee7a58a3c
Denis Žoljom <denis.zoljom@gmail.com>
2020-05-31 20:27:37 +0200

    Refactor the question controller

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -11,10 +11,9 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
+use App\Service\MarkdownHelper;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\Cache\CacheInterface;
 use Twig\Environment;
 
 /**
@@ -44,7 +43,7 @@ class QuestionController extends AbstractController
   /**
    * @Route("/questions/{slug}", name="app_question_show")
    */
-  public function show($slug, MarkdownParserInterface $markdownParser, CacheInterface $cache)
+  public function show($slug, MarkdownHelper $markdownHelper)
   {
     $answers = [
       'Make sure your cat is sitting `purrrfectly` still 🤣',
@@ -54,11 +53,7 @@ class QuestionController extends AbstractController
 
     $questionText = 'I\'ve been turned into a cat, any *thoughts* on how to turn back? While I\'m **adorable**, I don\'t really care for cat food.';
 
-    $parsedQuestionText = $cache->get('markdown_'.md5($questionText), function() use ($questionText, $markdownParser) {
-      return $markdownParser->transformMarkdown($questionText);
-    });
-
-    dump($cache);
+    $parsedQuestionText = $markdownHelper->parse($questionText);
 
     return $this->render('question/show.html.twig', [
       'question' => ucwords(str_replace('-', ' ', $slug)),
