d333ebec76f876a0ecc95308c19392e28056ea68
Denis Žoljom <denis.zoljom@gmail.com>
2020-06-03 18:15:06 +0200

    Cleanup

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -55,14 +55,11 @@ class QuestionController extends AbstractController
   /**
    * @Route("/questions/{slug}", name="app_question_show")
    */
-  public function show($slug, MarkdownHelper $markdownHelper, HubInterface $sentryHub)
+  public function show($slug, MarkdownHelper $markdownHelper)
   {
-    dump($sentryHub);
     if ($this->isDebug) {
       $this->logger->info('We are in debug mode');
     }
-
-    throw new \Exception('Bad stuff happened!');
 
     $answers = [
       'Make sure your cat is sitting `purrrfectly` still 🤣',
