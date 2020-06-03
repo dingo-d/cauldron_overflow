5e8f2fb038c56646c4eeddefe4b7fa8d65059981
Denis Žoljom <denis.zoljom@gmail.com>
2020-06-03 18:02:32 +0200

    Add sentry

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -13,6 +13,7 @@ namespace App\Controller;
 
 use App\Service\MarkdownHelper;
 use Psr\Log\LoggerInterface;
+use Sentry\State\HubInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
@@ -54,12 +55,15 @@ class QuestionController extends AbstractController
   /**
    * @Route("/questions/{slug}", name="app_question_show")
    */
-  public function show($slug, MarkdownHelper $markdownHelper)
+  public function show($slug, MarkdownHelper $markdownHelper, HubInterface $sentryHub)
   {
+    dump($sentryHub);
     if ($this->isDebug) {
       $this->logger->info('We are in debug mode');
     }
-    
+
+    throw new \Exception('Bad stuff happened!');
+
     $answers = [
       'Make sure your cat is sitting `purrrfectly` still 🤣',
       'Honestly, I like furry shoes better than MY cat',
