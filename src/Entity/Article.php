8d7e19a3683c3670157bf4a3e03953691069eee3
Denis Žoljom <denis.zoljom@gmail.com>
2020-06-12 11:35:48 +0200

    Add new entities
    Add Comment and question entities

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -10,83 +10,139 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Article
 {
-    /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
-     */
-    private $id;
+  /**
+   * @ORM\Id()
+   * @ORM\GeneratedValue()
+   * @ORM\Column(type="integer")
+   */
+  private $id;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $title;
+  /**
+   * @ORM\Column(type="string", length=255)
+   */
+  private $title;
 
-    /**
-     * @ORM\Column(type="string", length=100, unique=true)
-     */
-    private $slug;
+  /**
+   * @ORM\Column(type="string", length=100, unique=true)
+   */
+  private $slug;
 
-    /**
-     * @ORM\Column(type="text", nullable=true)
-     */
-    private $content;
+  /**
+   * @ORM\Column(type="text", nullable=true)
+   */
+  private $content;
 
-    /**
-     * @ORM\Column(type="datetime", nullable=true)
-     */
-    private $publishedAt;
+  /**
+   * @ORM\Column(type="datetime", nullable=true)
+   */
+  private $publishedAt;
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
+  /**
+   * @ORM\Column(type="string", length=255)
+   */
+  private $author;
 
-    public function getTitle(): ?string
-    {
-        return $this->title;
-    }
+  /**
+   * @ORM\Column(type="integer")
+   */
+  private $heartCount = 0;
 
-    public function setTitle(string $title): self
-    {
-        $this->title = $title;
+  /**
+   * @ORM\Column(type="string", length=255, nullable=true)
+   */
+  private $imageFilename;
 
-        return $this;
-    }
+  public function getId(): ?int
+  {
+    return $this->id;
+  }
 
-    public function getSlug(): ?string
-    {
-        return $this->slug;
-    }
+  public function getTitle(): ?string
+  {
+    return $this->title;
+  }
 
-    public function setSlug(string $slug): self
-    {
-        $this->slug = $slug;
+  public function setTitle(string $title): self
+  {
+    $this->title = $title;
 
-        return $this;
-    }
+    return $this;
+  }
 
-    public function getContent(): ?string
-    {
-        return $this->content;
-    }
+  public function getSlug(): ?string
+  {
+    return $this->slug;
+  }
 
-    public function setContent(?string $content): self
-    {
-        $this->content = $content;
+  public function setSlug(string $slug): self
+  {
+    $this->slug = $slug;
 
-        return $this;
-    }
+    return $this;
+  }
 
-    public function getPublishedAt(): ?\DateTimeInterface
-    {
-        return $this->publishedAt;
-    }
+  public function getContent(): ?string
+  {
+    return $this->content;
+  }
 
-    public function setPublishedAt(?\DateTimeInterface $publishedAt): self
-    {
-        $this->publishedAt = $publishedAt;
+  public function setContent(?string $content): self
+  {
+    $this->content = $content;
 
-        return $this;
-    }
+    return $this;
+  }
+
+  public function getPublishedAt(): ?\DateTimeInterface
+  {
+    return $this->publishedAt;
+  }
+
+  public function setPublishedAt(?\DateTimeInterface $publishedAt): self
+  {
+    $this->publishedAt = $publishedAt;
+
+    return $this;
+  }
+
+  public function getAuthor(): ?string
+  {
+    return $this->author;
+  }
+
+  public function setAuthor(string $author): self
+  {
+    $this->author = $author;
+
+    return $this;
+  }
+
+  public function getHeartCount(): ?int
+  {
+    return $this->heartCount;
+  }
+
+  public function setHeartCount(int $heartCount): self
+  {
+    $this->heartCount = $heartCount;
+
+    return $this;
+  }
+
+  public function getImageFilename(): ?string
+  {
+    return $this->imageFilename;
+  }
+
+  public function setImageFilename(?string $imageFilename): self
+  {
+    $this->imageFilename = $imageFilename;
+
+    return $this;
+  }
+
+  public function getImagePath()
+  {
+    return 'images/'.$this->getImageFilename();
+  }
 }
---
+++ src/Entity/Question.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\QuestionRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=QuestionRepository::class)
+ */
+class Question
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=100, unique=true)
+     */
+    private $slug;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $questionContent;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $publishedAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getQuestionContent(): ?string
+    {
+        return $this->questionContent;
+    }
+
+    public function setQuestionContent(string $questionContent): self
+    {
+        $this->questionContent = $questionContent;
+
+        return $this;
+    }
+
+    public function getPublishedAt(): ?\DateTimeInterface
+    {
+        return $this->publishedAt;
+    }
+
+    public function setPublishedAt(?\DateTimeInterface $publishedAt): self
+    {
+        $this->publishedAt = $publishedAt;
+
+        return $this;
+    }
+}
