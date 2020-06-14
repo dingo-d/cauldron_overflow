0153eb5cdec22563f9f5d10d972eeea3963b955e
Denis Žoljom <denis.zoljom@gmail.com>
2020-06-14 15:23:58 +0200

    Style and implement pagination in the manage answers view

--- src/Controller/AnswersAdminController.php
+++ src/Controller/AnswersAdminController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\AnswerRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,14 +13,20 @@ class AnswersAdminController extends AbstractController
     /**
      * @Route("/admin/answers", name="answers_admin")
      */
-    public function index(AnswerRepository $repository, Request $request)
+    public function index(AnswerRepository $repository, Request $request, PaginatorInterface $paginator)
     {
         $q = $request->query->get('q');
 
-        $answers = $repository->findAllWIthSearch($q);
+        $queryBuilder = $repository->getWithSearchQueryBuilder($q);
+
+        $pagination = $paginator->paginate(
+            $queryBuilder, /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            10 /*limit per page*/
+        );
 
         return $this->render('answers_admin/index.html.twig', [
-            'answers' => $answers
+            'pagination' => $pagination
         ]);
     }
 }
--- src/Repository/AnswerRepository.php
+++ src/Repository/AnswerRepository.php
@@ -37,25 +37,23 @@ class AnswerRepository extends ServiceEntityRepository
 
     /**
      * @param string|null $term
-     * @return Comment[]
      */
-    public function findAllWIthSearch(?string $term)
+    public function getWithSearchQueryBuilder(?string $term): QueryBuilder
     {
         $qb = $this->getOrCreateQueryBuilder();
 
         $qb->innerJoin('an.author', 'u')
             ->innerJoin('an.question', 'q')
             ->addSelect('q')
-            ->addSelect('u');
+            ->addSelect('u')
+            ->orderBy('an.vote', 'DESC');
 
         if ($term) {
             $qb->andWhere('an.content LIKE :term OR u.name LIKE :term OR q.title LIKE :term')
                 ->setParameter('term', '%' . $term . '%');
         }
 
-        return $qb->orderBy('an.vote', 'DESC')
-            ->getQuery()
-            ->getResult();
+        return $qb;
     }
 
 }
